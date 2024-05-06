07f41b6d33a686583cbffc51fe064e73d1951c99
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-05-06 07:02:56 -0500

    creacion de persistencias

---
+++ src/persistence/persistence.module.ts
@@ -0,0 +1,15 @@
+import { Global, Module } from '@nestjs/common';
+import {configType} from '@nestjs/config';
+import {MongooseModule} from '@nestjs/mongoose';
+
+@Global()
+@Module({
+    imports: [
+        MongooseModule.forFeatureAsync({
+            useFactory: () => {
+                
+            }
+        })
+    ]
+})
+export class PersistenceModule {}
