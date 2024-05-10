3f4486caead148831f12d0bca88d5dc65e177e39
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-05-10 13:10:09 -0500

    agregar pipes al main y creacion de pipe parsonalizada

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 4000;
 
-  app.setGlobalPrefix('/v1/api');
-  await app.listen(port);
-  console.log(`Running app on :http://localhost:${port}/v1/api`);
+  app.useGlobalPipes(new ValidationPipe(
+    {
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }
+  ));
   // bloqueos de consultas externas restringe la capacidad de una aplicación o sistema para realizar solicitudes o consultas a recursos externos,
   app.enableCors();
-
+  
   const config = new DocumentBuilder()
   .setTitle('API_MINDSET')
   .setDescription('authentication with NestJs and MongoDB ')
@@ -20,6 +25,10 @@ async function bootstrap() {
   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+  
+  app.setGlobalPrefix('/v1/api');
+  console.log(`Running app on :http://localhost:${port}/v1/api`);
 
+  await app.listen(port);
 }
 bootstrap();
